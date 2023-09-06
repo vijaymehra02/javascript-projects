39d75f995e1e71adf34e0c0a8bcdcd77177778db
vijay bahadur singh mehra <vijaybahadursinghmehra@gmail.com>
2023-09-06 22:46:57 +0530

    movie app on working

---
+++ js mini projects/movie app/script.js
@@ -0,0 +1,32 @@
+// popuals movies API 
+const APIURL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const IMGPATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+
+const main = document.querySelector(".main");
+
+const init = async (api) => {
+    const responce = await fetch(api);
+    const dataobj = await responce.json()
+    const dataarray = Array.from(dataobj)
+    dataobj.results.forEach((element) => {
+        console.log(element)
+        const container = document.createElement("div");
+        container.classList= ("container b")
+        container.innerHTML = `
+      <div class="img b">
+                  <img src="${IMGPATH + element.poster_path }" alt="img">
+              </div>
+              <div class="pop b">
+                  <div class="tital b">
+                      <h2 class="b">${element.title}</h2>
+                      <span class="b">${element.vote_average}</span>
+                  </div>
+                  <p class="b">${element.overview}</p>
+              </div>
+      `;
+
+      main.appendChild(container)
+    });
+}
+init(APIURL);
